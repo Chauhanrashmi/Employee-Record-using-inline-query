e9cf3354d53eb604add35c3f62619a4264b1a077
Rashmi Chauhan <chauhanrashmi1998@gmail.com>
2018-07-06 20:29:45 -0700

    first commit

---
+++ Default.aspx
@@ -0,0 +1,83 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style2 {
+            font-size: x-large;
+            font-weight: 700;
+        }
+        .auto-style3 {
+            font-size: x-large;
+        }
+        .auto-style4 {
+            font-size: x-large;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <center>
+    <form id="form1" runat="server">
+    <div>
+    
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Font-Bold="True" Font-Italic="True" Font-Size="Large" Text="EMPLOYEE DETAILS" style="font-size: x-large" BorderStyle="Dotted"></asp:Label>
+        <br />
+        <br />
+        <br />
+    
+        <strong>
+    
+        <asp:Label ID="Label1" runat="server" Text="Emp no" CssClass="auto-style3"></asp:Label>
+        </strong><span class="auto-style3"><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </strong></span><strong>
+        <asp:TextBox ID="TextBox1" runat="server" placeholder="Enter Employee id" CssClass="auto-style4"></asp:TextBox>
+        <br class="auto-style3" />
+        <br class="auto-style3" />
+        <asp:Label ID="Label2" runat="server" Text="Emp name" CssClass="auto-style3"></asp:Label>
+        </strong><span class="auto-style3"><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </strong></span><strong>
+        <asp:TextBox ID="TextBox2" runat="server" placeholder="Enter Employee name" CssClass="auto-style4"></asp:TextBox>
+        <br class="auto-style3" />
+        <br class="auto-style3" />
+        <asp:Label ID="Label3" runat="server" Text="Emp address" CssClass="auto-style3"></asp:Label>
+        </strong><span class="auto-style3"><strong>&nbsp;&nbsp;&nbsp;
+        </strong></span><strong>
+        <asp:TextBox ID="TextBox3" runat="server" placeholder="Enter Employee address" CssClass="auto-style4"></asp:TextBox>
+        <br class="auto-style3" />
+        <br class="auto-style3" />
+        <asp:Label ID="Label4" runat="server" Text="Emp sal" CssClass="auto-style3"></asp:Label>
+        </strong><span class="auto-style3"><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </strong></span><strong>
+        <asp:TextBox ID="TextBox4" runat="server" placeholder="Enter Employee salary" CssClass="auto-style4"></asp:TextBox>
+        </strong>
+        <br class="auto-style2" />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="Save" Width="91px" CssClass="auto-style3" OnClick="Button1_Click1" />
+        <span class="auto-style3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </span>
+        <asp:Button ID="Button2" runat="server" Text="Update" Width="113px" CssClass="auto-style3" OnClick="Button2_Click1" />
+        <span class="auto-style3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </span>
+        <asp:Button ID="Button3" runat="server" Text="Delete" Width="92px" CssClass="auto-style3" OnClick="Button3_Click" />
+        <span class="auto-style3">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </span>
+        <asp:Button ID="Button4" runat="server" Text="Display" Width="114px" CssClass="auto-style3" OnClick="Button4_Click" />
+    
+        <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" Width="293px"></asp:ListBox>
+    
+        <br />
+    
+    </div>
+    </form>
+        </center>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        con.ConnectionString = ConfigurationManager.ConnectionStrings["cn"].ConnectionString;
+        if (con.State == ConnectionState.Closed)
+        {
+            con.Open();
+        }
+    }
+
+   
+
+    private void record()
+    {
+        TextBox1.Text = "";
+        TextBox2.Text = string.Empty;
+        TextBox3.Text = string.Empty;
+        TextBox4.Text = string.Empty;
+        TextBox1.Focus();
+    }
+
+
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        Dis_rec();
+    }
+
+    private void Dis_rec()
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "Select * from emp";
+        cmd.Connection = con;
+        SqlDataReader dr;
+        dr = cmd.ExecuteReader();
+        ListBox1.DataTextField = "Empname";
+        ListBox1.DataValueField = "Empno";
+        ListBox1.DataSource = dr;
+        ListBox1.DataBind();
+        dr.Close();
+        cmd.Dispose();
+    }
+
+
+    protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "select * from emp where Empno=@Empno";
+        cmd.Connection = con;
+        cmd.Parameters.Add("@Empno",SqlDbType.Int).Value=Convert.ToInt32(ListBox1.SelectedValue);
+        SqlDataReader dr;
+        dr = cmd.ExecuteReader();
+        if (dr.HasRows)
+        {
+            dr.Read();
+            TextBox1.Text = dr["Empno"].ToString();
+            TextBox2.Text = dr["Empname"].ToString();
+            TextBox3.Text = dr["Empad"].ToString();
+            TextBox4.Text = dr["Empsal"].ToString();
+        }
+        dr.Close();
+        cmd.Dispose();
+    }
+
+   
+
+    protected void Button1_Click1(object sender, EventArgs e)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "insert emp values(@Empno,@Empname,@Empad,@Empsal)";
+        cmd.Connection = con;
+        cmd.Parameters.Add("@Empno", SqlDbType.Int).Value = Convert.ToInt32(TextBox1.Text);
+        cmd.Parameters.Add("@Empname", SqlDbType.VarChar, 50).Value = TextBox2.Text;
+        cmd.Parameters.Add("@Empad", SqlDbType.VarChar, 50).Value = TextBox3.Text;
+        cmd.Parameters.Add("@Empsal", SqlDbType.Int).Value = Convert.ToInt32(TextBox4.Text);
+        cmd.ExecuteNonQuery();
+        cmd.Dispose();
+        Dis_rec();
+        record();
+    }
+
+    protected void Button2_Click1(object sender, EventArgs e)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "update emp set Empname=@Empname,Empad=@Empad,Empsal=@Empsal where Empno=@Empno";
+        cmd.Connection = con;
+        cmd.Parameters.Add("@Empno", SqlDbType.Int).Value = Convert.ToInt32(TextBox1.Text);
+        cmd.Parameters.Add("@Empname", SqlDbType.VarChar, 50).Value = TextBox2.Text;
+        cmd.Parameters.Add("@Empad", SqlDbType.VarChar, 50).Value = TextBox3.Text;
+        cmd.Parameters.Add("@Empsal", SqlDbType.Int).Value = Convert.ToInt32(TextBox4.Text);
+        cmd.ExecuteNonQuery();
+        cmd.Dispose();
+        Dis_rec();
+        record();
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        SqlCommand cmd = new SqlCommand();
+        cmd.CommandText = "delete from emp where Empno=@Empno";
+        cmd.Connection = con;
+        cmd.Parameters.Add("@Empno",SqlDbType.Int).Value=Convert.ToInt32(TextBox1.Text);
+        cmd.ExecuteNonQuery();
+        cmd.Dispose();
+        Dis_rec();
+        record();
+    }
+}
